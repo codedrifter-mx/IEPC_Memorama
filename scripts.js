b6ebce785564f6a2ffce1dfdad268e3d858bad10
alfredo-flores <alfredo.f.g.156@gmail.com>
2022-07-19 11:44:16 -0500

    voltear sound fix 7

--- scripts.js
+++ scripts.js
@@ -256,6 +256,7 @@ function unflipCards() {
     try {
         setTimeout(() => {
 
+            tssByWord('unflip')
 
             firstCard.classList.remove('flip');
             secondCard.classList.remove('flip');
